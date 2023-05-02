36358d6bdb6df92f5a98abe75e7fa4bbae4f0859
abdelbasset4 <benatmani1999@gmail.com>
2023-05-02 16:06:44 +0200

    finish footer design and create scroll to top button

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector('.logo img')
 const list = document.querySelector('nav ul')
 const btn = document.getElementById('menu-btn')
+const scrollToTop = document.querySelector('.scroll-to-top')
 
 window.addEventListener('scroll', function() {
     const navBar = document.getElementById('navbar');
@@ -17,8 +18,18 @@ window.addEventListener('scroll', function() {
       } else {
         logo.src = "./images/logo/logo-white.svg";
       }
+     
+      // show scroll to top btn
+      this.scrollY >= 1000 ? scrollToTop.classList.add("show") : scrollToTop.classList.remove("show");
   });
 
+
+scrollToTop.onclick = function(){
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('flex')
